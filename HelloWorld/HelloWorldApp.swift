01d04c5f0fcfd12fa5d09dd4d591f1aea297d7c9
Marcelo Alves <marceloalves1295@gmail.com>
2022-12-03 07:24:33 -0800

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Marcelo Alves on 20/11/22.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
